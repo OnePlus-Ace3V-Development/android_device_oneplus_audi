9c10d4c693114d9d066d30f601d27dedcd4c07ac
SaKongA <2623493447@qq.com>
2024-07-21 12:18:02 +0800

    audi: Initial skeleton device tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_audi.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_audi-eng \
+    lineage_audi-userdebug \
+    lineage_audi-user
